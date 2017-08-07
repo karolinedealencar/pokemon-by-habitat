8f18a156fc1e53351efa0d06cb96b7e8ed0b7b63
Karoline <karolinedalencar@gmail.com>
2017-08-07 17:09:21 -0300

    finishing project

---
+++ js/main.js
@@ -0,0 +1,49 @@
+const $select = document.querySelector('[data-js="habitat-select"]')
+const $main = document.querySelector('[data-js="pokemon-result"]')
+
+window.addEventListener('load', searchHabitats)
+
+function searchHabitats() {
+  const habitatPromise = fetch('https://pokeapi.co/api/v2/pokemon-habitat/')
+
+  habitatPromise
+  .then(data => data.json())
+  .then(data => showHabitats(data.results))
+}
+
+function showHabitats(habitats) {
+  const habitatsHTML = `
+      ${habitats.map(habitat =>
+        `<option data-js="">${habitat.name}</option>`
+      ).join('')}
+  `
+
+  $select.insertAdjacentHTML('beforeend', habitatsHTML)
+  $select.addEventListener('click', function() {
+    if (this.value !== 'select') {
+      listenOptions(this.value)
+    }
+  })
+}
+
+function listenOptions(habitat) {
+  const selectPromise = fetch(`https://pokeapi.co/api/v2/pokemon-habitat/${habitat}`);
+
+  selectPromise
+  .then(data => data.json())
+  .then(data => showPokemons(data.pokemon_species));
+}
+
+function showPokemons(allPokemons) {
+  const markUp = `
+    <ul class="pokemon-result__list">
+      ${allPokemons.map(pokemon =>
+        `<li class="pokemon-result__item">
+            ${pokemon.name}
+         </li>`
+      ).join('')}
+    </ul>
+  `
+
+  $main.innerHTML = markUp
+}
